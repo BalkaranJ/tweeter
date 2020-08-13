00d79337d0032b3bb782de71296047fd14f1e357
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-08-13 23:53:19 +0000

    Added Responsive Design, Fixed Tweet Boxes, Added functionality and warning sign for passing character limit, add safeguard against tweeted javascript

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,113 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const escape =  function(str) {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+//How an escape function works line by line
+
+/* <script>$('body').empty()</script>
+
+<div></div>
+
+<div><script>$('body').empty()</script>
+</div>
+
+returns as string using .innerHTML script $() */
+
+
+ //Create a function that will generatre the DOM structure for a tweet, given a tweet object
+//return a tweet <article> element which gets prepended (to show the newest tweet) to the Tweet Box section in the html file (.tweets)
+const createTweetElement = function(tweetData) {
+  return `
+<article class='the-box'>
+    <div class='tweet-header'>
+      <div class='tweet-username'>
+        <img class="user-image" src="${tweetData.user.avatars}"/>
+        <h6 class='username'>${tweetData.user.name}</h6>
+      </div>
+      <div class='tweet-handle'>
+        <h6 class='handle'>${tweetData.user.handle}</h6>
+      </div>
+    </div>
+    <div class='tweet-message'>
+      <h4 class='user-tweet'>${escape(tweetData.content.text)}</h4>
+    </div>
+    <hr class='tweet-divider'>
+    <footer class='tweet-footer'>
+      <p>${tweetData.created_at}</p>
+      <div class='the-images'>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article> 
+`;
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const tweetHTML = createTweetElement(tweet);
+    $('.tweets').prepend(tweetHTML);
+  }
+}
+
+
+const loadtweets = function() {
+//Getting data using Jquery
+$.getJSON('http://localhost:8080/tweets')
+  .then((tweets) => {
+    renderTweets(tweets);
+  })
+}
+
+$(document).ready(function(){
+  //Loads tweets on refresh
+  loadtweets();
+
+  //Form Submission Using Jquery
+  const $form = $(".the-form");
+  $form.submit(function(event) {
+    event.preventDefault();
+    const serialized = $(this).serialize();
+
+    $.ajax('/tweets/', {method:'POST', data:serialized})
+    .then(() => {
+      $('.tweets').empty();
+      loadtweets();
+    });
+  }); 
+});
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,31 @@
 //Responsible for character counter
 
 //.ready ensures that all the html documentation is loaded before the javascript scripts run
-
-//JQuery Steps
-// Step 1: Target 
-// Step 2: Add Eventlistener 
-// Step 3: Retarget (optional)
-// Step 4: Effect
-
 $(document).ready(() => {
   //keyup event counts a character when the key is released
   $('textarea').keyup(function(){
     const tweetLength = this.value.length;
     const maxCharacterLimit = 140;
+    const charsLeft = maxCharacterLimit - tweetLength;
     let counter = $('.counter')
-    
-    counter.val(maxCharacterLimit - tweetLength);
-    console.log(counter.val());
+    counter.val(charsLeft);
 
     // if (counter.val() < 0) {
     //   counter.css('color', 'red');
     // } else {
     //   counter.css('color', '#545149');
     // }
-
     // Ternary operator
-    counter.val() < 0 ? counter.css('color', 'red')  : counter.css('color', '#545149');
+    charsLeft < 0 ? counter.css('color', 'red')  : counter.css('color', '#545149');
+
+    //Incase the character limit for a tweet is passed
+      //This will ensure that the user understands why the tweet won't submit, and it will disable the tweet button while the counter displays red and negative.
+    if (charsLeft < 0) {
+      $('#error-tweet-length').css('visibility', 'visible');
+      $('#tweet-button').attr('disabled', true);
+    } else {
+      $('#error-tweet-length').css('visibility', 'none');
+      $('#tweet-button').attr('disabled', false);
+    }
   });
 });
